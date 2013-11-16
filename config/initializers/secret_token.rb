a09c693d1054c4842ad288ef23a4cd0498d26297
Philip Simnonds <phillipsimmonds@linuxmint.linuxmint-domain>
2013-11-17 05:28:54 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HzrV2::Application.config.secret_key_base = '20913b5e537284a2fc5897e2750263d5c4e2da919081a93e422cb6ab8f40d1d2bcd6e076e4f9dc28019e7a793f732ea33d33f1f5b9817e565fd95b873bd9b1e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HzrV2::Application.config.session_store :cookie_store, key: '_hzr_v2_session'
